91784b9e7dc91a9cb45c7b5d993c9d302b9b81b3
Stefan Spijkerman <spijkerman2@hotmail.com>
2025-04-10 20:35:20 +0200

    remove unused files

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 // Reexport the native module. On web, it will be resolved to ExpoDatalogicScannerModule.web.ts
 // and on native platforms to ExpoDatalogicScannerModule.ts
-export { default } from './ExpoDatalogicScannerModule';
-export { default as ExpoDatalogicScannerView } from './ExpoDatalogicScannerView';
-export * from  './ExpoDatalogicScanner.types';
+import ExpoDatalogicScannerModule from "./ExpoDatalogicScannerModule";
+
+export function hello(): string {
+  return ExpoDatalogicScannerModule.hello();
+}
